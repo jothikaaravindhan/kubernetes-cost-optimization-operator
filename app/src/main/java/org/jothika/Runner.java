db326bb16ad97e84a52a3afc4ff25cf40947a6dc
jothikaaravindhan@gmail.com <jothikaaravindhan@gmail.com>
2025-04-10 07:11:48 +0100

    Add project base skeleton

---
+++ app/src/main/java/org/jothika/Runner.java
@@ -0,0 +1,18 @@
+package org.jothika;
+
+import io.javaoperatorsdk.operator.Operator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Runner {
+
+    private static final Logger log = LoggerFactory.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+        Operator operator = new Operator();
+        //This is where the reconcilers needs to be registered
+        operator.start();
+        log.info("Operator started.");
+    }
+}
